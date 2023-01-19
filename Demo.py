fef145cf83014faf03b759559ba9774f5df10ae1
Amogh Agastya <46256520+amoghagastya@users.noreply.github.com>
2023-01-19 21:22:09 +0530

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -105,7 +105,7 @@ def chat(
     size="ada",
     debug=False,
     max_tokens=400,
-    stop_sequence=["User:"],
+    stop_sequence=None,
 ):
     """
     Answer a question based on the most similar context from the dataframe texts
